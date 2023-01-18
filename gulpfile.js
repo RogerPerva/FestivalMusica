3f5883a13794f3bb3c324965c4ed9fbce5c491fa
Rogelio U <ulisescodi@gmail.com>
2023-01-18 14:58:43 -0600

    Añadiendo PLUMBER y normalize

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,18 @@
    
 ///!!!!!!!!!!!!!!!!!!! Para ejecutar los archivos de gulp, utilizamos 'npx gulp (nombre de tarea)'
 
-            const {src, dest, watch} = require("gulp");
+    const {src, dest, watch} = require("gulp");
     const sass = require ("gulp-sass")(require("sass"));
+    const plumber = require("gulp-plumber");
 
 function css(done){
     
     
     src('src/scss/**/*.scss')    //Identificar el archivo de SASS. Saber donde esta
+        .pipe(plumber()) //lo que hace plumber es que en caso de que haya errores no tenga problemas y detenga el workflow
         .pipe( sass())//Compilarlo.
         .pipe(dest("build/css")); //Almacenarla en el disco duro
+        
     //PIPE es una accion que se realiza despues de otra, es decir, en cadena.
     //Se pueden tener multiples pipes.
     
